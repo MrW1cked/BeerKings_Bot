ac672256f92074f06d938520b6c91f39c33c6d44
Nuno Sousa <nusousa@mercadona.com>
2024-08-21 17:45:06 +0100

    first comit

--- BeerKings-Bot/src/main/java/com/back/beerkings/repositories/LanguageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.back.beerkings.repositories;
-
-import com.back.beerkings.models.database.LanguageMO;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LanguageRepository extends JpaRepository<LanguageMO, Long> {
-}
---
+++ src/test/java/com/back/beerkings/BeerKingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.beerkings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerKingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
